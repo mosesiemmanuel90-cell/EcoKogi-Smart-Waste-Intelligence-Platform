bdbb7fe1c26585eac7ebf4dc35eed08c1b8d05c7
Moses Emmanuel <mosesiemmanuel90@gmail.com>
2026-07-27 19:02:58 +0100

    Fix build errors and prepare project for deployment'

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,18 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import checker from "vite-plugin-checker";
-// @ts-expect-error - No type declarations for custom plugin
-import clearLogPlugin from "./dala-internal-vite-clear-log-plugin.js";
-
 import dns from "node:dns";
-
 dns.setDefaultResultOrder("verbatim");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    clearLogPlugin(),
-    react(),
-    tailwindcss(),
-    checker({
-      typescript: true,
-    }),
-  ],
+ plugins: [
+  react(),
+  tailwindcss(),
+  checker({
+    typescript: true,
+  }),
+],
   server: {
     port: 3000,
     host: true,
